488001879e4eeb7db258b787006e2f4074a09be7
atlantis1005 <152236848+atlantis1005@users.noreply.github.com>
2025-04-23 22:57:00 -0400

    dropdown works
    next adding dividers

--- scripts.js
+++ scripts.js
@@ -35,13 +35,122 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-  document.getElementById('fly-aleutian').addEventListener('click', () => {
+  document.getElementById('211 Street').addEventListener('click', () => {
     map.flyTo({
-        center: [-170.88841, 54.53183],
-        zoom: 4.33,
-        duration: 1500
+        center: [-73.86389, 40.87792],
+        zoom: 17.56,
+        duration: 1800
+        
     })
 })
+document.getElementById('Bathgate').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.89792, 40.84491],
+        zoom: 17.67,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Clinton').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.88848, 40.84623],
+        zoom: 17.76,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Rhodebeck').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.88990, 40.82163],
+        zoom: 17.54,
+        duration: 1800
+        
+    })
+})
+
+document.getElementById('Target Bronx').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.92749, 40.83304],
+        zoom: 17.70,
+        duration: 1800
+        
+    })
+})
+
+document.getElementById('Willis').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.92021, 40.81230],
+        zoom: 17.82,
+        duration: 1800
+        
+    })
+})
+
+document.getElementById('Bridge Plaza').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.98465, 40.69791],
+        zoom: 18.40,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Greene Acres').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.95804, 40.68799],
+        zoom: 18.20,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Jane Bailey').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.95857, 40.68792],
+        zoom: 18.17,
+        duration: 1800
+        
+    })
+})
+document.getElementById('McLeods').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.90535, 40.67283],
+        zoom: 18.14,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Scholes').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.94466, 40.70835],
+        zoom: 18.09,
+        duration: 1800
+        
+    })
+})
+document.getElementById('La Casita').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.93759, 40.79974],
+        zoom: 18.25,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Los Amigos').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.93214, 40.79597],
+        zoom: 18.60,
+        duration: 1800
+        
+    })
+})
+document.getElementById('Rodale').addEventListener('click', () => {
+    map.flyTo({
+        center: [-73.93440, 40.79429],
+        zoom: 18.33,
+        duration: 1800
+        
+    })
+})
+
   
   // Close the dropdown menu if the user clicks outside of it
   window.onclick = function(event) {
